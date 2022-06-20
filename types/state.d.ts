9e04cc04f1e44799002d39deeb22a5f0a338c12f
Sune Simonsen <sune@we-knowhow.dk>
2022-06-20 16:42:10 +0200

    Don't commit types to github

--- types/state.d.ts
+++
@@ -1,7 +0,0 @@
-export function addStateListener(listener: import('./shared').StateListener): void;
-export function removeStateListener(listener: import('./shared').StateListener): void;
-export function subscribables(): import('./shared').Subscribables;
-export function flush(): void;
-export function observable<T>(initialValue: T, options?: import("./shared").SubscribableOptions<T>): import("./shared").Observable<T>;
-export function computed<T>(cb: () => T, options?: import("./shared").SubscribableOptions<T>): import("./shared").Computed<T>;
-//# sourceMappingURL=state.d.ts.map
