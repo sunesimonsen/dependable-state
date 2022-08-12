1fceabd74ee0018c79a1472a7cc84fff19647f10
Sune Simonsen <sune@we-knowhow.dk>
2022-08-12 09:03:14 +0200

    Initializing two subscribables with the same id, will return the same

--- src/state.js
+++ src/state.js
@@ -152,6 +152,12 @@ const registerUpdate = (fn) => {
  */
 export const observable = (initialValue, options = {}) => {
   const { id, isEqual = Object.is } = options;
+
+  if (id && dependableState._references.has(id)) {
+    let cached = dependableState._references.get(id).deref();
+    if (cached) return cached;
+  }
+
   let value = initialValue;
   let prevValue = initialValue;
 
@@ -235,6 +241,12 @@ export const track = (cb) => {
  */
 export const computed = (cb, options = {}) => {
   const { id, isEqual = Object.is } = options;
+
+  if (id && dependableState._references.has(id)) {
+    let cached = dependableState._references.get(id).deref();
+    if (cached) return cached;
+  }
+
   let value = null;
   let prevValue = null;
   let active = false;
